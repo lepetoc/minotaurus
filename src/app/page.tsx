6004d482a842e92ec0d9ff01c1aac1f4f16a6c7a
Xelame <m2afkeb9@duck.com>
2026-06-10 14:04:05 +0200

    feat(chat):add chat screen on the root route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function HomePage() {
-  return 'Hello World'
+import { getServerSession } from 'next-auth'
+import { ChatInterface } from './chat-interface'
+import { authOptions } from '@/lib/auth'
+
+export default async function HomePage() {
+  const session = await getServerSession(authOptions)
+
+  return <ChatInterface username={session?.user?.name ?? 'Moi'} />
 }
